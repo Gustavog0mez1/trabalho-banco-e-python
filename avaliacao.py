9e4678bd8769f465b4c22a4bcee481a1eab6fe22
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-24 08:54:07 -0300

    Update avaliacao.py

--- avaliacao.py
+++ avaliacao.py
@@ -2,11 +2,13 @@ import random
 
 print("=== AVALIAÇÕES LIVRO ===")
 titulo_livro = input("Título do livro: ")
-nota_livro = input("Nota do livro: ")
-data_avaliacao = input("Data avaliação: ")
-perfil_avaliador = input("Perfil avaliador: ")
-if not nota_livro.replace('.', '').isdigit():
-    print("ERRO: Nota deve conter apenas números!")
+nota_livro = input("Nota do livro : ")
+data_avaliacao = input("Data da avaliação: ")
+perfil_avaliador = input("Perfil do avaliador: ")
+
+if not nota_livro.isdigit() or len(nota_livro) > 2:
+    print("ERRO: Nota deve conter apenas números e no máximo 2 dígitos!")
 else:
     id_avaliacao = random.randint(1, 1000)
-    print(f"Avaliação cadastrada! ID:{id_avaliacao} Livro:{titulo_livro} Nota:{nota_livro} Data:{data_avaliacao} Avaliador:{perfil_avaliador}")
+    print(f"Avaliação cadastrada! ID:{id_avaliacao} Livro:{titulo_livro} Nota:{nota_livro}/10 Data:{data_avaliacao} Avaliador:{perfil_avaliador}")
+    
