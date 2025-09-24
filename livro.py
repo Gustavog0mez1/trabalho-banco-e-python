218fbc84d478e7e932518eb360ab718fb9b23434
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-24 08:29:16 -0300

    Create livro.py

---
+++ livro.py
@@ -0,0 +1,12 @@
+import random
+
+print("===LIVRO ===")
+titulo_livro = input("Título do livro: ")
+valor = input("Valor: ")
+estado_conservacao = input("Estado de conservação: ")
+autor = input("Autor: ")
+if not valor.replace('.', '').isdigit():
+    print("ERRO: Valor deve conter apenas números!")
+else:
+    id_livro = random.randint(1, 1000)
+    print(f"Livro cadastrado! ID:{id_livro} Título:{titulo_livro} Valor:R${valor} Estado:{estado_conservacao} Autor:{autor}")
