da53926fd917548ddaa133e23647be6b1bd46194
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-24 08:47:42 -0300

    Create doacao.py

---
+++ doacao.py
@@ -0,0 +1,14 @@
+import random
+
+print("=== DOAÇÃO LIVROS ===")
+titulo_livro = input("Título do livro: ")
+valor = input("Valor: ")
+estado_livro = input("Estado do livro: ")
+data_doacao = input("Data doação (DDMMAA): ")
+if not valor.replace('.', '').isdigit():
+    print("ERRO: Valor deve conter apenas números!")
+elif len(data_doacao) != 6 or not data_doacao.isdigit():
+    print("ERRO: Data deve ter 6 dígitos (DDMMAA)!")
+else:
+    id_doacao = random.randint(1, 1000)
+    print(f"Doação cadastrada! ID:{id_doacao} Livro:{titulo_livro} Valor:R${valor} Estado:{estado_livro} Data:{data_doacao}")
