3ebd59520e82f2f58877cd3c1df6ee56911adfbf
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-25 10:20:12 -0300

    Update login_cliente.py

--- login_cliente.py
+++ login_cliente.py
@@ -3,9 +3,9 @@ import random
 print("=== LOGIN CLIENTE ===")
 email = input("Email: ")
 cpf = input("CPF: ")
-if not cpf.isdigit():
-    print("ERRO: CPF deve conter apenas números!")
+if not cpf.isdigit() or len(cpf) != 11:
+    print("ERRO: CPF deve conter apenas números e ter 11 dígitos!")
 else:
     senha = input("Senha: ")
-    id_cliente= random.randint(1, 1000)
+    id_cliente = random.randint(1000000000, 9999999999)
     print(f"Login realizado! ID:{id_cliente} Email:{email} CPF:{cpf}")
