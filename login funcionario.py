b762e0521d1ca6a77d9e44ee42befc0c2f391f70
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-25 10:17:05 -0300

    Update login funcionario.py

--- login funcionario.py
+++ login funcionario.py
@@ -3,9 +3,9 @@ import random
 print("=== LOGIN FUNCIONÁRIO ===")
 email = input("Email: ")
 cpf = input("CPF: ")
-if not cpf.isdigit():
-    print("ERRO: CPF deve conter apenas números!")
+if not cpf.isdigit() or len(cpf) != 11:
+    print("ERRO: CPF deve conter apenas números e ter 11 dígitos!")
 else:
     senha = input("Senha: ")
-    id_funcionario = random.randint(1, 1000)
+    id_funcionario = random.randint(1000000000, 9999999999)
     print(f"Login realizado! ID:{id_funcionario} Email:{email} CPF:{cpf}")
