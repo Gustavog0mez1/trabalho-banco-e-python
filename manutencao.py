2c219dc8b6c0a29582098cc73318866c73d0e5f7
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-25 10:22:10 -0300

    Update manutencao.py

--- manutencao.py
+++ manutencao.py
@@ -8,5 +8,5 @@ data_manutencao = input("Data da manutenção: ")
 if len(data_manutencao) > 6 or not data_manutencao.isdigit():
     print("ERRO: Data deve conter apenas números e no máximo 6 dígitos!")
 else:
-    id_manutencao = random.randint(1, 1000)
+    id_manutencao = random.randint(1000000000, 9999999999)
     print(f"Manutenção cadastrada! ID:{id_manutencao} Livro:{titulo_livro} Estado:{estado_livro} Data:{data_manutencao}")
