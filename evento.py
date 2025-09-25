85f816dafc67a2e29f8a79773a68691b9f6877db
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-25 10:13:34 -0300

    Update evento.py

--- evento.py
+++ evento.py
@@ -6,5 +6,5 @@ data_evento = input("Data do evento: ")
 if len(data_evento) > 6 or not data_evento.isdigit():
     print("ERRO: Data deve conter apenas números e no máximo 6 dígitos!")
 else:
-    id_evento = 1234567890  # ID fixo
+    id_evento = 1234567890 
     print(f"Evento cadastrado! ID:{id_evento} Funcionário:{nome_funcionario} Evento:{nome_evento} Data:{data_evento}")
