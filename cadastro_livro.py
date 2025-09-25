fad21f3f50c7a8728847818af61dfb882457d327
Gustavog0mez1 <159037936+Gustavog0mez1@users.noreply.github.com>
2025-09-25 10:08:54 -0300

    Update cadastro_livro.py

--- cadastro_livro.py
+++ cadastro_livro.py
@@ -4,8 +4,9 @@ print("=== CADASTRO LIVRO ===")
 titulo = input("Título: ")
 valor = input("Valor: ")
 estado_conservacao = input("Estado de conservação: ")
+autor = input("Autor: ")
 if not valor.replace('.', '').isdigit():
     print("ERRO: Valor deve conter apenas números!")
 else:
-    id_livro = random.randint(1, 1000)
-    print(f"Livro cadastrado! ID:{id_livro} Título:{titulo} Valor:R${valor} Estado:{estado_conservacao}")
+    id_livro = random.randint(1000000000, 9999999999)
+    print(f"Livro cadastrado! ID:{id_livro} Título:{titulo} Valor:R${valor} Estado:{estado_conservacao} Autor:{autor}")
